fbb29c115fec0bc6dd0a750ba38a9d54e26ca8c6
Arshpreet Singh <arshsomal100@gmail.com>
2026-04-27 12:20:44 +0530

    added lab est

---
+++ LABEST/Ques2.java
@@ -0,0 +1,31 @@
+package LABEST;
+
+interface SocialMedia{
+    public void post();
+}
+
+class Whatsapp implements SocialMedia{
+    @Override
+    public void post() {
+        System.out.println("Whatsapp");
+    }
+}
+class Instagram implements SocialMedia{
+    @Override
+    public void post() {
+        System.out.println("Instagram");
+    }
+}
+class Facebook implements SocialMedia{
+    @Override
+    public void post() {
+        System.out.println("Facebook");
+    }
+}
+
+public class Ques2{
+
+    public static void sharePost(SocialMedia media){
+        media.post();
+    }
+}
